2d2cb415cba78f91ab95f77e40809a3d14d0d0a8
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-10-25 16:04:19 +0300

    Learned IconButton and PopScope for buttons of appbar and navigation for turn back page

--- lib/pages/sayfaB.dart
+++ lib/pages/sayfaB.dart
@@ -15,44 +15,62 @@ class _SayfaBState extends State<SayfaB> {
       appBar: AppBar(
         title: Text("Sayfa B"),
         backgroundColor: Colors.deepOrange,
+        /// appbardaki geridönüş tuşu
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: (){
+            print("Appbar geri tuşu tıklandı");
+            Navigator.of(context).popUntil((route)=>route.isFirst);
+          },
+        ),
+        ///
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Sayfa B'ya hoşgeldiniz"),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.pop(context); /// Geldiği sayfaya döner.
-              },
-              child: Text("Geldiğin sayfaya Dön"),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.pink,
+      body:
+      /// alt taraftai geridönüş tuşu
+      PopScope(
+        canPop: false, ///Kendi kodumuzu eklemek için bunu yazdık
+        onPopInvokedWithResult: (bool didPop, Object? result){
+          print("Navigation'daki geri tuşu tıklandı");
+          Navigator.of(context).popUntil((route)=>route.isFirst);
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Sayfa B'ya hoşgeldiniz"),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.pop(context); /// Geldiği sayfaya döner.
+                },
+                child: Text("Geldiğin sayfaya Dön"),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.pink,
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.of(context).popUntil((route)=>route.isFirst);
-                /// Yeni sayfa açmadan direk en başa yani anasayfaya döner.
-              },
-              child: Text("Anasayfa'ya Dön"),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.pink,
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.of(context).popUntil((route)=>route.isFirst);
+                  /// Yeni sayfa açmadan direk en başa yani anasayfaya döner.
+                },
+                child: Text("Anasayfa'ya Dön"),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.pink,
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>SayfaA()));
-              },
-              child: Text("Sayfa A'ya Git"),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.pink,
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SayfaA()));
+                },
+                child: Text("Sayfa A'ya Git"),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.pink,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
