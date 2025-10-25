904af5640a0e1c91e2db7b5e9ff952636088dc91
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-10-25 14:14:19 +0300

    Learned creating a page of StatelessWidget by other file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:android_flutter_working_structure/sayfa2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Working Structure'),
+      // home: const MyHomePage(title: 'Flutter Working Structure'),
+      // home: Sayfa1(),
+      home: Sayfa2(),
     );
   }
 }
@@ -32,9 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  int sayac=0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,21 +43,30 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    sayac++;
-                  });
-                },
-                child: Text("Tıkla")
-            ),
-            Text("Deger : $sayac"),
-          ],
-        ),
       )
     );
   }
 }
+
+class Sayfa1 extends StatelessWidget {
+  const Sayfa1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Page of StatelessWidget"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hello World"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
